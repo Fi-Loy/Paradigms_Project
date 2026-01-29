9f7354e3bb1e5d419ba4af5517a055b4f246a5d1
swimordie22 <pbell031@uottawa.ca>
2026-01-29 16:13:58 -0500

    Complete Program class implementation    - Replaced matchedResidents array with ArrayList    - Implemented member(), rank(), leastPreferred(), and addResident() methods

--- Java/src/Program.java
+++ Java/src/Program.java
@@ -2,6 +2,8 @@
 // Winter 2026
 // Robert Laganiere, uottawa.ca
 
+import java.util.ArrayList;
+
 // this is the (incomplete) Program class
 public class Program {
 	
@@ -10,15 +12,16 @@ public class Program {
 	private int quota;
 	private int[] rol;
 
-	Resident[] matchedResidents;
+	private ArrayList<Resident> matchedResidents = new ArrayList<>();
 
-	
+
 	// constructs a Program
     public Program(String id, String n, int q) {
 	
 		programID= id;
 		name= n;
 		quota= q;
+
 	}
 
     // the rol in order of preference
@@ -34,11 +37,15 @@ public class Program {
 	}
 
 	/**
-	 * Checks if resident is matched to this Program
+	 * Checks if resident is in ROL of current program.
 	 * @param residentId
 	 */
 	public boolean member(int residentId){
-		//TODO: Finish implementation
+		for (int id : rol) {
+			if (residentId == id) {
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -48,7 +55,11 @@ public class Program {
 	 * included in the ROL. 
 	 */
 	public int rank(int residentID){
-		//TODO: Finish implementation
+		for (int i = 0; i < rol.length;i++) {
+			if (rol[i] == residentID) {
+				return i;
+			}
+		}
 		return -1;
 	}
 
@@ -56,19 +67,44 @@ public class Program {
 	 * Returns the resident with the highest rank
 	 * (lowest preference)
 	 */
-	public Resident leastPreferred(){
-		//TODO: Finish implementation
-		return null;
-	}
+	public Resident leastPreferred() {
+		Resident highestResident = null;
+		int highestRank = -1;
 
+		for (Resident resident : matchedResidents) {
+			int currentRank = rank(resident.getResidentID());
+
+			if (currentRank > highestRank) {
+				highestRank = currentRank;
+				highestResident = resident;
+			}
+		}
+		return highestResident;
+	}
 	/**
-	 * Adds Resdient to the match list of the program if
+	 * Adds Resident to the match list of the program if
 	 * the program has not reached its quota or if the
 	 * resident is preferred over an already matched
 	 * resident. 
 	 */
 	public void addResident(Resident resident){
-		//TODO: Finish implementation
+		int rank = rank(resident.getResidentID());
+		if (rank == -1) return;
+
+		if (matchedResidents.size() < quota) {
+			matchedResidents.add(resident);
+			resident.setMatchedProgram(this, rank);
+			return;
+		}
+		Resident leastPreferred = leastPreferred();
+		int leastPreferredRank = rank(leastPreferred.getResidentID());
+
+		if (rank < leastPreferredRank) {
+			matchedResidents.remove(leastPreferred);
+			leastPreferred.clearMatch();
+			matchedResidents.add(resident);
+			resident.setMatchedProgram(this,rank);
+		}
 	}
 
 }
